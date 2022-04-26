b2053af2d5749d7d282160f86a058a5d0a48a0b9
MuhRiswan <m.riswan0921@gmail.com>
2022-04-26 19:03:36 +0700

    Tugas-9

---
+++ tugas-9/mahasiswa/proses.php
@@ -0,0 +1,28 @@
+<?php
+include "../database/connection.php";
+
+    
+    $nama = $_POST['nama']; 
+    $jurusan = $_POST['jurusan'];
+
+   $query = mysqli_query($conn, "insert into mahasiswa set nama = '$nama', jurusan = '$jurusan'");
+
+   if ($query) {
+    $message = "Data berhasil ditambahkan!";
+    
+    $respone = array(
+    'status' => "OK",
+    'message' => $message
+    );
+    echo json_encode($respone,JSON_PRETTY_PRINT);
+
+    // $message = urlencode($message);
+    // header("Location:mahasiswa.php?message={$message}");
+    } else {
+        $message = "Data gagal ditambahkan!";
+        // $message = urlencode($message);
+        // header("Location:mahasiswa.php?message={$message}");
+    }
+
+
+?>
